a72b92fc91148d91a39aef7320e0a35807abeb36
Luciano <lucianobonilla27@gmail.com>
2023-03-24 16:23:26 -0300

    commit manejando arreglo con indices

---
+++ tp2_1_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define N 20
+
+
+int main()
+{
+   int i;
+    double vt[N];
+    for(i = 0;i<N; i++)
+    {
+    vt[i]=1+rand()%100;
+    printf("%.2f\n", vt[i]);
+    }
+    return 0;
+}
